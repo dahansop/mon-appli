33cd1e139e2f627a2775d57b6a026c80ce44f98a
dahansop <125454964+dahansop@users.noreply.github.com>
2023-02-16 17:00:24 +0100

    Update App.java

--- src/main/java/org/exemple/demo/App.java
+++ src/main/java/org/exemple/demo/App.java
@@ -6,8 +6,8 @@ package org.exemple.demo;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
     }
 }
